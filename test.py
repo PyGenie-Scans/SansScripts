569cddbec036508651687b63a3a131410a176dc1
Adam Washington <adam.washington@stfc.ac.uk>
2018-05-11 15:43:33 +0100

    Properly import from source directory

--- test.py
+++ test.py
@@ -1,8 +1,8 @@
-import Instrument
-from genie import gen
+import src.Instrument
+from src.genie import gen
 
-Instrument.measure_changer("BT", title="test", sanstrans="TRANS", uamps=3)
-Instrument.measure_changer("BT", title="test", sanstrans="SANS", uamps=15)
-Instrument.measure_changer("BT", title="test", sanstrans="SANS", uamps=15)
+src.Instrument.measure_changer("BT", title="test", sanstrans="TRANS", uamps=3)
+src.Instrument.measure_changer("BT", title="test", sanstrans="SANS", uamps=15)
+src.Instrument.measure_changer("BT", title="test", sanstrans="SANS", uamps=15)
 
 print(gen.mock_calls)
