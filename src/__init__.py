fc6becdc2b464c1fed1a3f9a3a58783b9eb36318
Adam Washington <adam.washington@stfc.ac.uk>
2018-05-17 14:29:01 +0100

    Massive lint removal

--- src/__init__.py
+++ src/__init__.py
@@ -1,7 +1,15 @@
+"""This module creates unified access to all SANS instruments.
+
+The best way of using the module is with a single
+
+    from SansScripting import *
+"""
+
+
 from socket import gethostname
 from functools import wraps
 from .Instrument import ScanningInstrument
-from Util import user_script
+from .Util import user_script
 
 SCANNING = None
 
@@ -12,21 +20,22 @@ def is_instrument(title):
 
 
 if is_instrument("Larmor"):
-    from Larmor import Larmor
+    from .Larmor import Larmor
     SCANNING = Larmor()
-if is_instrument("Zoom"):
-    from .Zoom import Zoom
-    SCANNING = Zoom()
+# if is_instrument("Zoom"):
+#     from .Zoom import Zoom
+#     SCANNING = Zoom()
 if not SCANNING:
     # Default to Larmor if we can't find an instrument
     # This is mostly for development
-    from Larmor import Larmor
+    from .Larmor import Larmor
     SCANNING = Larmor()
 
 
 def _local_wrapper(method):
     @wraps(getattr(SCANNING, method))
     def inner(*args, **kwargs):
+        """Call the method without the object"""
         return getattr(SCANNING, method)(*args, **kwargs)
     if not inner.__doc__ and hasattr(ScanningInstrument, method):
         inner.__doc__ = getattr(ScanningInstrument, method).__doc__
