c83fd5795a117d7febed837d7dbb9b87c3ec9872
Adam Washington <adam.washington@stfc.ac.uk>
2018-05-18 15:34:13 +0100

    Bump version number

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -26,7 +26,7 @@ author = u'Adam Washington'
 # The short X.Y version
 version = u''
 # The full version, including alpha/beta/rc tags
-release = u'0.0.2'
+release = u'0.0.3'
 
 
 # -- General configuration ---------------------------------------------------
