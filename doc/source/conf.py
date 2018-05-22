b28afa232c45f8c23f923f6f88fc2f73636d474b
Adam Washington <rprospero@gmail.com>
2018-05-22 10:05:11 +0100

    Update version tag

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -24,9 +24,9 @@ copyright = u'2018, Adam Washington'
 author = u'Adam Washington'
 
 # The short X.Y version
-version = u''
+version = u'0.1'
 # The full version, including alpha/beta/rc tags
-release = u'0.0.3'
+release = u'0.1.0'
 
 
 # -- General configuration ---------------------------------------------------
