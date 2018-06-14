e642e02ae5387e7354a3196aa84cad905858f7b8
Adam Washington <adam.washington@stfc.ac.uk>
2018-06-14 16:12:06 +0100

    Record test coverage

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,4 +1,4 @@
 ((nil . ((projectile-project-compilation-cmd . "cd doc;make html")
-	 (projectile-project-run-cmd . "python -m doctest doc/source/tutorial.rst")
+	 (projectile-project-run-cmd . "coverage run --source src -m doctest doc/source/tutorial.rst; and coverage html")
 	 (projectile-project-test-cmd . "flake8 src; and pylint src")
 					     )))
