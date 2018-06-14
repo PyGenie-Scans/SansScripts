e70b50e340e913fe0d24c38d829eca3819874d9c
Adam Washington <adam.washington@stfc.ac.uk>
2018-06-14 15:13:13 +0100

    Don't try to turn on the detector unless specifically asked.

--- src/Instrument.py
+++ src/Instrument.py
@@ -432,8 +432,10 @@ class ScanningInstrument(object):
         """
         self._needs_setup()
         if not self.detector_lock() and not self.detector_on() and not trans:
-            warning("The detector was off.  Turning on the detector")
-            self.detector_on(True)
+            raise RuntimeError(
+                "The detector is off.  Either turn on the detector or "
+                "use the detector_lock(True) to indicate that the detector "
+                "is off intentionally")
         self.set_default_dae(dae, trans)
         self._setup_measurement(trans, blank)
         self.set_measurement_label(title)
